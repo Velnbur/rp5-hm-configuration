c0cf212fd41f0e64a500260062d4d08ad3e15bb9
Velnbur <kyrylo.baybula@distributedlab.com>
2025-01-01 12:10:13 +0200

    Add username + more programs

--- home.nix
+++ home.nix
@@ -3,4 +3,25 @@
   programs.bat.enable = true;
 
   home.stateVersion = "25.05";
+  home.username = "velnbur";
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+    options = [ "--cmd" "cd" ];
+  };
+
+  programs.btop = {
+    enable = true;
+    settings = {
+      vim_keys = true;
+    };
+  };
+
+  programs.eza.enable = true;
 }
