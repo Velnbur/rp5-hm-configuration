fed204f36533db470d8dcc56fa31a6704a1c0374
Velnbur <kyrylo.baybula@distributedlab.com>
2025-01-01 14:20:40 +0200

    Get rid of samba

--- home.nix
+++ home.nix
@@ -44,40 +44,4 @@
       ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=8080 --save-path=/media/torrents --profile=${home.homeDirectory}/.config/qbittorrent";
     };
   };
-
-  systemd.user.services.samba = {
-    Unit = {
-      Description = "smbd service";
-      Documentation = [ "man:smbd(1)" ];
-    };
-
-    Service = {
-      Type = "notify";
-      ExecStart = "${pkgs.samba}/sbin/smbd --foreground --no-process-group --configfile=${home.homeDirectory}/.config/samba/samba.conf";
-    };
-  };
-
-  home.file.".config/samba/samba.conf" = {
-    enable = true;
-    text = ''
-      [global]
-           server role = standalone server
-           map to guest = Bad User
-           usershare allow guests = yes
-           hosts allow = 192.168.0.0/16
-           hosts deny = 0.0.0.0/0
-           log file = ${home.homeDirectory}/.local/logs/samba/%m.log
-           state directory = ${home.homeDirectory}/.local/state/samba
-           cache directory = ${home.homeDirectory}/.cache/samba
-           lock directory = ${home.homeDirectory}/.local/state/samba
-           pid directory = ${home.homeDirectory}/.local/state/samba
-
-
-      [torrents]
-           comment = Shared torrents
-           guest ok = yes
-           path = /media/torrents
-           read only = yes
-    '';
-  };
 }
